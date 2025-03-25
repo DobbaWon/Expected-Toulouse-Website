969ecf0ebee5e781832a3ef05b96fa35facfd2a3
DobbaWon <charlie.j.dobson@outlook.com>
2025-03-25 17:17:22 +0000

    Updated css

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,11 +1,14 @@
 import "./Header.css";
 
 import Navbar from "./Navbar/Navbar";
+import { Link } from "react-router-dom";
 
 function Header(){
     return (
         <header className="header">
-            <img src="/Logo.jpg" alt="Logo" />
+            <Link to="/">
+                <img src="/Logo.jpg" alt="Logo" />
+            </Link>
             <Navbar />
         </header>
     );
--- src/Header/Navbar/Navbar.jsx
+++ src/Header/Navbar/Navbar.jsx
@@ -5,23 +5,10 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav>
-       <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/shop">Club Shop</Link>
-        </li>
-        <li>
-          <Link to="/fixtures">Fixtures and Results</Link>
-        </li>
-        <li>
-          <Link to="/players">Players</Link>
-        </li>
-        <li>
-          <Link to="/history">History</Link>
-        </li>
-      </ul>
+      <Link to="/shop">Club Shop</Link>
+      <Link to="/fixtures">Fixtures and Results</Link>
+      <Link to="/players">Players</Link>
+      <Link to="/history">History</Link>
     </nav>
   );
 };
