c8a7339b2f876070c0b0de98b21a753af5dc534f
DobbaWon <charlie.j.dobson@outlook.com>
2025-03-25 17:35:35 +0000

    Moved stuff around

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import Footer from './Footer/Footer';
-import Header from './Header/Header';
-import HomePage from './HomePage/HomePage';
-import History from './History/History';
-import Fixtures from './Fixtures/Fixtures';
-import Players from './Players/Players';
-import ClubShop from './ClubShop/ClubShop';
+import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
+import HomePage from './Pages/HomePage/HomePage';
+import History from './Pages/History/History';
+import Fixtures from './Pages/Fixtures/Fixtures';
+import Players from './Pages/Players/Players';
+import ClubShop from './Pages/ClubShop/ClubShop';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
