729ca2a59de40be190f5c9e2886700e11dfea830
DobbaWon <charlie.j.dobson@outlook.com>
2025-04-01 19:40:23 +0100

    Added background to History Page, updated Footer

--- src/Pages/History/History.jsx
+++ src/Pages/History/History.jsx
@@ -3,7 +3,7 @@ import "./History.css";
 function History() {
     return (
         <div>
-            
+            <img src="Dick.png" alt="background" className="background"/>
         </div>
     );
 }
