17880aef8cf2554b187c5fe6c1db264e219f53e6
DobbaWon <charlie.j.dobson@outlook.com>
2025-04-10 18:19:51 +0100

    Added boilerplate history page

--- src/Pages/History/History.jsx
+++ src/Pages/History/History.jsx
@@ -1,9 +1,23 @@
 import "./History.css";
+import history from "./history.json";
 
 function History() {
     return (
-        <div>
-            <img src="Dick.png" alt="background" className="background"/>
+        <div className="container">
+            <div className="paragraph-one">
+                <img src="" className="image-one"/>
+                <p className="text-one">{history.paragraph_one}</p>
+            </div>
+
+            <div className="paragraph-two">
+                <p className="text-two">{history.paragraph_two}</p>
+                <img src="" className="image-two"/>
+            </div>
+
+            <div className="paragraph-three">
+                <img src="" className="image-three"/>
+                <p className="text-three">{history.paragraph_three}</p>
+            </div>
         </div>
     );
 }
